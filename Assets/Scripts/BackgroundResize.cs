419fbc928ead5ecab64a9156fca2c46c8d0c3294
Tomás Vélez Núñez <43381969+txvnt@users.noreply.github.com>
2020-05-17 19:15:55 -0400

    Project init

---
+++ Assets/Scripts/BackgroundResize.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+ public class BackgroundResize : MonoBehaviour {
+
+     void Update () {
+        var sr = GetComponent<SpriteRenderer>();
+        if (sr == null) return;
+        
+        transform.localScale = new Vector3(1,1,1);
+        
+        var width = sr.sprite.bounds.size.x;
+        var height = sr.sprite.bounds.size.y;
+        
+        var worldScreenHeight = Camera.main.orthographicSize * 2.0;
+        var worldScreenWidth = worldScreenHeight / Screen.height * Screen.width;
+        
+        transform.localScale = new Vector3((float)(worldScreenWidth / width), (float)(worldScreenHeight / height), 1);
+     }
+ }
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D rigidbody; 
+    public float speed;
+    public float jumpForce;
+
+    public GameObject prefab;
+
+    float tiempo = 5;
+    public Text contador;
+    bool contando = true;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        GameObject go = Instantiate(prefab); //Instanciar prefab de plataforma
+        contador.text = "Tiempo: 5";
+    }
+
+    void Update()
+    {
+        float movementX = Input.GetAxis("Horizontal");
+        float movementY = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(movementX * speed, movementY * jumpForce);
+        rigidbody.AddForce(movement);
+
+        if(contando){
+            tiempo -= Time.deltaTime;
+            contador.text = "Tiempo: " + (tiempo).ToString();
+        }
+
+        if(tiempo < 0) {
+            Debug.Log("Perdiste");
+            contando = false;
+        }
+    }
+}
